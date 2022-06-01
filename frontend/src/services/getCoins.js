a5e2308b93dcba8dc8ba98e6f84a358c8e12435a
disher.io <drew@disher.io>
2022-06-01 01:28:05 -0400

    removed some MORE unwanted things

--- frontend/src/services/getCoins.js
+++ frontend/src/services/getCoins.js
@@ -4,7 +4,7 @@ const getCoins = async (count) => {
       {
         "method": "GET",
         "headers": {
-        "x-access-token": "coinrankingf4f50f46ee7fff173f7a0c22cdb44bf2369f30d9f4983ab5",
+        "x-access-token": "",
         "content-type": "application/json",
         "accept": "application/json"
       }
